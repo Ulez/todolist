72632c030ed2483458269bb490c86762ba8d0a22
Ulez <1104128773@qq.com>
2017-09-22 15:56:37 +0800

    v-for 显示TODOlist

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="app">
     <h1>{{title}}</h1>
+    <ol>
+      <li v-for="item in items">
+        {{ item.label }}
+      </li>
+    </ol>
   </div>
 </template>
 
 <script>
 export default {
-  data: function(){
-    return{
-      title:'This is todolist'
+  data: function() {
+    return {
+      title: 'This is todolist',
+      items: [
+        { label: 'coding', finished: false },
+        { label: 'dota', finished: true },
+        { label: 'read', finished: false }
+      ]
     }
   }
 }
