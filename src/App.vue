88b2355680bee77e71ff855a8b3e6031a81a72e9
Ulez <1104128773@qq.com>
2017-09-22 17:10:55 +0800

    输入item，显示到TODOlist

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>{{title}}</h1>
+    <input v-model="newItem" @keyup.enter="onEnter">
     <ol>
       <li v-for="item in items" v-bind:class="{ finished: item.finished }" v-on:click="reverse(item)">
         {{ item.label }}
@@ -14,16 +15,20 @@ export default {
   data: function() {
     return {
       title: 'This is todolist',
-      items: [
-        { label: 'coding', finished: false },
-        { label: 'dota', finished: true },
-        { label: 'read', finished: false }
-      ]
+      items: [],
+      newItem: ''
     }
   },
   methods: {
     reverse: function(item) {
       item.finished = !item.finished
+    },
+    onEnter: function() {
+      this.items.push({
+        label: this.newItem,
+        finished: false
+      })
+      this.newItem = ''
     }
   }
 }
